5c8c1dd11f0a98ab6fcf7aa713d8f595ac488231
Roman Shishkin <romashkin.2001@yandex.ru>
2026-03-16 01:06:16 +0500

    feat: Added classes for composite id entities

---
+++ testsys-infra/database/src/main/kotlin/tech/testsys/infra/database/jpa/entity/JpaCompositeEntity.kt
@@ -0,0 +1,43 @@
+package tech.testsys.infra.database.jpa.entity
+
+import jakarta.persistence.Embeddable
+import jakarta.persistence.EmbeddedId
+import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.CreationTimestamp
+import java.io.Serializable
+import java.time.Instant
+
+/**
+ * Base abstract class for composite (multi-column) primary keys used with [JpaCompositeEntity].
+ *
+ * Subclasses must be annotated with [Embeddable] and are recommended to be Kotlin `data class`es,
+ * which automatically provide the [equals] and [hashCode] implementations required by the JPA spec.
+ *
+ * @since %CURRENT_VERSION%
+ */
+@MappedSuperclass
+abstract class JpaCompositeId : Serializable
+
+/**
+ * Base abstract class for all JPA entities with a composite (multi-column) primary key.
+ *
+ * Uses an [EmbeddedId] of type [T] instead of a simple auto-generated [Long] identifier.
+ *
+ * Subclasses should be annotated with `@Entity` and provide a concrete [JpaCompositeId] subclass
+ * as the type parameter.
+ *
+ * @param T the composite key type, must extend [JpaCompositeId].
+ * @property id the composite primary key.
+ * @property createdAt moment of record creation (UTC), populated automatically on first persist.
+ *
+ * @since %CURRENT_VERSION%
+ */
+@MappedSuperclass
+abstract class JpaCompositeEntity<T : JpaCompositeId>(
+
+    @EmbeddedId
+    val id: T,
+
+    @CreationTimestamp
+    val createdAt: Instant = Instant.now(),
+)

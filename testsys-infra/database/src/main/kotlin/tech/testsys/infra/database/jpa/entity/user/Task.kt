a0bfecc6f7608dc8a335af84f1f6b96d2028e00c
Roman Shishkin <romashkin.2001@yandex.ru>
2026-03-16 02:00:05 +0500

    feat: Updated task jpa entities

--- testsys-infra/database/src/main/kotlin/tech/testsys/infra/database/jpa/entity/user/Task.kt
+++ testsys-infra/database/src/main/kotlin/tech/testsys/infra/database/jpa/entity/user/Task.kt
@@ -1,21 +1,31 @@
 package tech.testsys.infra.database.jpa.entity.user
 
+import jakarta.persistence.Embeddable
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
 import jakarta.persistence.MappedSuperclass
+import tech.testsys.infra.database.jpa.entity.JpaCompositeEntity
+import tech.testsys.infra.database.jpa.entity.JpaCompositeId
 import tech.testsys.infra.database.jpa.entity.JpaEntity
 
 /**
+ * Supported programming languages for TRIK Studio tasks.
+ *
+ * @see tech.testsys.domain.model.task.TrikSupportedLanguage
  * @since %CURRENT_VERSION%
  */
 enum class TrikSupportedLanguageJpaEnum {
     PYTHON,
     JAVA_SCRIPT,
-    VISUAL_LANGUAGE
+    VISUAL_LANGUAGE;
 }
 
 /**
+ * JPA entity representing a judgment order domain entity.
+ *
+ * @see tech.testsys.domain.model.task.JudgmentOrder
+ * @see tech.testsys.domain.model.task.JudgmentOrderData
  * @since %CURRENT_VERSION%
  */
 @Entity
@@ -25,6 +35,9 @@ class JudgmentOrderJpaEntity(
 ) : JpaEntity()
 
 /**
+ * Base JPA entity for file-based domain entities.
+ *
+ * @see tech.testsys.domain.model.task.FileData
  * @since %CURRENT_VERSION%
  */
 @MappedSuperclass
@@ -33,6 +46,10 @@ abstract class FileJpaEntity(
 ) : JpaEntity()
 
 /**
+ * JPA entity representing a test domain entity.
+ *
+ * @see tech.testsys.domain.model.task.Test
+ * @see tech.testsys.domain.model.task.TestData
  * @since %CURRENT_VERSION%
  */
 @Entity
@@ -45,6 +62,10 @@ class TestJpaEntity(
 ) : FileJpaEntity(uploadedFilename)
 
 /**
+ * JPA entity representing an exercise domain entity.
+ *
+ * @see tech.testsys.domain.model.task.Exercise
+ * @see tech.testsys.domain.model.task.ExerciseData
  * @since %CURRENT_VERSION%
  */
 @Entity
@@ -57,6 +78,10 @@ class ExerciseJpaEntity(
 ) : FileJpaEntity(uploadedFilename)
 
 /**
+ * JPA entity representing a solution domain entity.
+ *
+ * @see tech.testsys.domain.model.task.Solution
+ * @see tech.testsys.domain.model.task.SolutionData
  * @since %CURRENT_VERSION%
  */
 @Entity
@@ -66,20 +91,38 @@ class SolutionJpaEntity(
     val language: TrikSupportedLanguageJpaEnum
 ) : FileJpaEntity(uploadedFilename)
 
-@IdClass
-class TestToTaskId(
-
-)
+@Embeddable
+data class TestToTaskId(
+    val testId: Long,
+    val taskId: Long,
+) : JpaCompositeId()
 
 /**
+ * JPA entity representing a test to task association domain entity.
+ *
  * @since %CURRENT_VERSION%
  */
 @Entity
-class TestsToTasksJpaEntity(
+class TestToTaskJpaEntity(
+    id: TestToTaskId,
+) : JpaCompositeEntity<TestToTaskId>(id)
 
-) : JpaEntity()
+@Embeddable
+data class DeveloperSolutionToTaskId(
+    val developerSolutionId: Long,
+    val taskId: Long,
+) : JpaCompositeId()
+
+@Entity
+class DeveloperSolutionToTaskJpaEntity(
+    id: TestToTaskId,
+) : JpaCompositeEntity<TestToTaskId>(id)
 
 /**
+ * JPA entity representing a task domain entity.
+ *
+ * @see tech.testsys.domain.model.task.Task
+ * @see tech.testsys.domain.model.task.TaskData
  * @since %CURRENT_VERSION%
  */
 @Entity
@@ -87,8 +130,13 @@ class TaskJpaEntity(
     val ownerId: Long,
     val name: String,
     val description: String,
-    val exerciseId: Long
+    val exerciseId: Long,
+    val trikStudioVersionImage: String,
+    val trikStudioVersionName: String,
+) : JpaEntity()
 
-    // TODO: developerSolutions
-
+@Entity
+class DeveloperSolutionJpaEntity(
+    val solutionId: Long,
+    val expectedVerdictId: Long,
 ) : JpaEntity()

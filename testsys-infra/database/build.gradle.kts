65c23148133476b1293e5fb116f6a3fa4f708ecd
Roman Shishkin <romashkin.2001@yandex.ru>
2026-04-26 05:01:57 +0500

    feat: Updated build

--- testsys-infra/database/build.gradle.kts
+++ testsys-infra/database/build.gradle.kts
@@ -10,4 +10,8 @@ dependencies {
     implementation(project(":testsys-domain"))
     implementation(libs.bundles.database.implementation)
     runtimeOnly(libs.postgresql)
+
+    testImplementation(libs.bundles.test.implementation)
+    testImplementation(libs.bundles.database.test.implementation)
+    testRuntimeOnly(libs.bundles.test.runtime)
 }

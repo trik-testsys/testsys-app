f51ec69a817c0adeee9884ee230ac3385761d1f7
Roman Shishkin <romashkin.2001@yandex.ru>
2026-05-14 14:39:57 +0500

    feat: Updated contracts

--- testsys-domain/src/main/kotlin/tech/testsys/domain/contract/Persistence.kt
+++ testsys-domain/src/main/kotlin/tech/testsys/domain/contract/Persistence.kt
@@ -6,6 +6,15 @@ import tech.testsys.domain.model.LazyEntity
 import tech.testsys.domain.model.LazyEntityList
 import tech.testsys.domain.model.LazyNullableEntity
 
+interface EntityFinder<Id : DomainId, Entity : DomainEntity<Id>> {
+
+    fun findById(id: Id): Entity?
+    fun findByIds(ids: List<Id>): List<Entity>
+//    fun findFiltered(filterParams: FilterParams): List<Entity>
+}
+
+//interface FilterParams
+
 interface EntityLoader<Id : DomainId, Entity : DomainEntity<Id>> {
 
     fun load(field: LazyEntity<Id, Entity>): Entity
@@ -23,4 +32,6 @@ interface EntitySaver<Data, Id : DomainId, Entity : DomainEntity<Id>> {
 }
 
 interface EntityRepository<Data, Id : DomainId, Entity : DomainEntity<Id>> :
-    EntitySaver<Data, Id, Entity>, EntityLoader<Id, Entity>
+    EntitySaver<Data, Id, Entity>,
+    EntityLoader<Id, Entity>,
+    EntityFinder<Id, Entity>
